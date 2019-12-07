3c88f94e38daa3d9bcd3493012c884953f1dcb87
Tali Gutman <talig@talig-macbookpro2.roam.corp.google.com>
2019-12-07 11:07:58 -0500

    Day 7 final

--- 2019/talig/day7/day7.py
+++ 2019/talig/day7/day7.py
@@ -78,7 +78,6 @@ def ExecuteInstructions(input_array, inputs, outputs):
     new_i, output = op(input_array, i, input_array[i]/100)
     if output != None:
       outputs.put(output)
-      return False
     i = new_i
     op_num = input_array[i] % 100
     op = GetOp(op_num, inputs)
@@ -110,6 +109,8 @@ def ChainQueues():
   for i in range(3, -1, -1):
     # Set queues for Amp #i to be a new input queue, and the next amp's input as output.
     amp_queues[i] = [Queue(), amp_queues[i+1][0]]
+  # Set the input to A to be the output of E.
+  amp_queues[0] = [amp_queues[4][1], amp_queues[0][1]]
   return amp_queues
 
 def RunAmplifiers(program, inputs):
@@ -128,10 +129,20 @@ def RunAmplifiersFeedback(program, inputs):
   for i in range(5): # Set the phase
     amps[i].input_queue.put(inputs[i])
   amps[0].input_queue.put(0) # For the first AMP, second input is 0.
-  done = False
-  while not done:
-    for i in range(5): # This is easier that A-E.
-      done = ExecuteInstructions(amps[i].program, amps[i].input_queue, amps[i%5].output_queue)
+  threads = []
+  for i in range(5): # This is easier that A-E.
+    x = threading.Thread(target=ExecuteInstructions, 
+                         args=(amps[i].program, 
+                               amps[i].input_queue, 
+                               amps[i%5].output_queue))
+    threads.append(x)
+    x.start()
+    
+  for i, thread in enumerate(threads):
+    #print 'Looking at AMP ', chr(ord('A')+i)
+    thread.join()
+    #print 'Done'
+
   return amps[4].output_queue.get()
 
 def TestComputer():
@@ -192,13 +203,39 @@ def TestAmplifiers():
       print 'Nope. Expected: ', test.expected_output, ' Received: ', output
 
 def TestAmplifiersFeedback():
-  return 0
+  tests = [
+          TestAmp([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,
+                   27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5], [9,8,7,6,5], 139629729), 
+          TestAmp([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,
+                   -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,
+                   53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10], [9,7,8,5,6], 18216)
+          ]
+          
+  for test in tests:
+    output = RunAmplifiersFeedback(test.program, test.input)
+    if output == test.expected_output:
+      print 'Pass'
+    else:
+      print 'Nope. Expected: ', test.expected_output, ' Received: ', output
 
 
 def Test():
   print 'Testing: '
   TestAmplifiers()
+  print 'Testing with feedback: '
+  TestAmplifiersFeedback()
   print '\n\n\n\n', '*'*30, '\n'
+
+def Run(permute, func, content):
+  max_output = 0
+  max_setting = []
+  for inp in itertools.permutations(permute):
+    output = func(content, inp)
+    if output > max_output:
+      max_output = output
+      max_setting = inp
+  
+  print func.func_name, ' ==> Max Output: ', max_output, ' with Setting: ', max_setting
 
 def main():
   Test()
@@ -212,15 +249,9 @@ def main():
   f.close()
   
   # (a)
-  max_output = 0
-  max_setting = []
-  for inp in itertools.permutations(range(5)):
-    output = RunAmplifiers(content, inp)
-    if output > max_output:
-      max_output = output
-      max_setting = inp
-  
-  print 'Max Output: ', max_output, ' with Setting: ', max_setting
+  Run(range(5), RunAmplifiers, content)
+  # (b)
+  Run(range(5,10), RunAmplifiersFeedback, content)
   
 if __name__== "__main__":
   main()
