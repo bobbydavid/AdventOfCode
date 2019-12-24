9df3f5a7c17c90dc4a3eb2d82d6ac4b91e5735ed
Tali Gutman <talig@talig-macbookpro2.roam.corp.google.com>
2019-12-24 15:03:38 -0500

    Day 24, part (b)

--- 2019/talig/day24/day24.py
+++ 2019/talig/day24/day24.py
@@ -1,4 +1,4 @@
-from grid import Grid, Point
+from grid import Grid, Point, PlutonianGrid
 import sys
 from os import system
 import copy
@@ -19,12 +19,10 @@ def advance_generation(eris):
     for x in range(new_eris.columns):
       location = Point(x,y)
       n = eris.count_neighbors(location)
-      if is_bug(eris.get(location)):
-        new_value = '#' if n == 1 else '.'
-        new_eris.set(location, new_value)
-      elif not is_bug(eris.get(location)):
-        new_value = '#' if (n == 1 or n==2) else '.'
-        new_eris.set(location, new_value)
+      new_value = '.'
+      if n == 1 or (not is_bug(eris.get(location)) and n == 2):
+        new_value = '#' 
+      new_eris.set(location, new_value)
   return new_eris
 
 def compute_biodiversity(eris):
@@ -38,13 +36,6 @@ def compute_biodiversity(eris):
   bin_str = '0b' + ''.join(binary)
   return int(bin_str,2)
 
-
-def expect(expected, real):
-  if expected == real:
-    print('Pass.')
-  else:
-    print('Nope! Expected:', expected, 'but got:', real)
-
 def find_first_repetition(eris):
   diversities = {}
   bio = compute_biodiversity(eris)
@@ -53,18 +44,72 @@ def find_first_repetition(eris):
   while not bio in diversities:
     diversities[bio] = current_eris
     current_eris = advance_generation(current_eris)
-    current_eris.render()
+    #current_eris.render()
     bio = compute_biodiversity(current_eris)
   return diversities, bio
+
+# (b)
+def get_plutonian_grid(filename):
+  eris_level_0 = get_grid(filename)
+  plutonian = PlutonianGrid(eris_level_0)
+  return plutonian
+
+def plutonian_advance(plutonian):
+  # First, note that we will probably need one level up and one level down.
+  # So add them to the current one.
+  plutonian.extend_levels()
+  levels = plutonian.get_levels_range()
+  new_plutonian = copy.deepcopy(plutonian)
+  for y in range(plutonian.rows):
+    for x in range(plutonian.cols):
+      location = Point(x, y)
+      if location == plutonian.center:
+        # No point in looking at this.
+        continue
+      for l in levels:
+        n = plutonian.count_neighbors(location, l)
+        new_value = '.'
+        if n == 1 or (not is_bug(plutonian.get(location, l)) and n == 2):
+          new_value = '#'
+        new_plutonian.set(location, l, new_value)
+  return new_plutonian
+
+def count_bugs(plutonian):
+  total = 0
+  for y in range(plutonian.rows):
+    for x in range(plutonian.cols):
+      location = Point(x, y)
+      if location == plutonian.center:
+        continue
+      for l in plutonian.levels:
+        total += 1 if is_bug(plutonian.get(location,l)) else 0
+  return total
+
+def run_minutes(minutes, plutonian):
+  plutonians = [plutonian]
+  for i in range(minutes):
+    plutonians.append(plutonian_advance(plutonians[i]))
+  return count_bugs(plutonians[-1])
+
+def expect(expected, real):
+  if expected == real:
+    print('Pass.')
+  else:
+    print('Nope! Expected:', expected, 'but got:', real)
     
 def test():
-  tests = {'test1.data': 2129920,
-          }
   # (a)
-  for t in tests:
+  test_A = {'test1.data': 2129920}
+  for t in test_A:
     eris = get_grid(t)
     d, bio = find_first_repetition(eris) 
-    expect(tests[t], bio)
+    expect(test_A[t], bio)
+  
+  # (b)
+  test_B = {'test1.data': (10, 99)}
+  for t in test_B:
+    res = run_minutes(test_B[t][0], get_plutonian_grid(t))
+    expect(test_B[t][1], res)
 
 def main():
   if (len(sys.argv) < 2):
@@ -79,7 +124,8 @@ def main():
   print('Biodiversity of first repeating state: ', b)
 
   # (b) ==>
-  
+  bugs = run_minutes(200, get_plutonian_grid(sys.argv[1]))
+  print('Bugs after 200 minutes in plutonian grid: ', bugs)
  
 if __name__== "__main__":
   main()
--- 2019/talig/day24/grid.py
+++ 2019/talig/day24/grid.py
@@ -1,4 +1,6 @@
 from os import system
+from collections import defaultdict
+import copy
 
 class Point:
   def __init__(self, x, y):
@@ -7,6 +9,9 @@ class Point:
 
   def __add__(self, o):
     return Point(self.x + o.x, self.y + o.y)
+  
+  def __sub__(self, o):
+    return Point(self.x - o.x, self.y - o.y)
 
   def __mul__(self, scalar):
     return Point(self.x * scalar, self.y * scalar)
@@ -36,15 +41,17 @@ class Grid:
     if not self.grid:
       for y in range(rows):
         self.grid.append(['.'] * columns)
-    print('Initialized grid with:', len(self.grid), 'rows X', len(self.grid[0]), 'columns')
 
   def set(self, point, value):
     self.grid[point.y][point.x] = value
   
   def get(self, point):
-    if point.y >= self.rows or point.y < 0 or point.x >= self.columns or point.x<0:
+    if not self.in_bounds(point):
       return '.'
     return self.grid[point.y][point.x]
+  
+  def in_bounds(self, point):
+    return point.y < self.rows and point.y >= 0 and point.x < self.columns and point.x>=0
 
   def count_neighbors(self, location):
     neighbors = 0
@@ -62,27 +69,84 @@ class Grid:
   def render(self):
     print(self)
 
+def get_new_level():
+  rows = 5
+  cols = 5
+  g = Grid(rows, cols, [])
+  g.set(Point(rows//2, cols//2), '?')
+  return g
+
 class PlutonianGrid:
   def __init__(self, g):
-    self.levels = []
     self.rows = g.rows
     self.cols = g.columns
+    self.center = Point(self.rows//2, self.cols//2)
+    self.levels = defaultdict(get_new_level)
     self.base = copy.deepcopy(g)
-    self.base.set(Point(self.rows//2, self.cols//2), '?')
+    self.base.set(self.center, '?')
+    self.levels[0] = self.base
+    self.neighbors_for_pos = {}
+    self.set_neighbors_for_pos()
 
-  def get(self, point):
-    if point.y >= self.rows or point.y < 0 or point.x >= self.columns or point.x<0:
-      # Need to down in the hierarchy
-      return '.'
-    return self.grid[point.y][point.x]
+  def set_neighbors_for_pos(self):
+    for y in range(self.rows):
+      for x in range(self.cols):
+        location = Point(x,y)
+        neighbors = [(0, location + d) for d in DIRECTIONS]
+        # Remove the center point if present:
+        if (0, self.center) in neighbors:
+          neighbors.remove((0, self.center))
 
-  def count_neighbors(self, location):
+        new_neighbors = []
+        # Don't change the list you're iterating over... sigh.
+        for t in neighbors:
+          # If it's out of bounds, replace it with its level up counterpart.
+          if not self.base.in_bounds(t[1]):
+            # get the direction this is in.
+            d = t[1] - location
+            new_neighbors.append((-1, self.center + d)) 
+          else:
+            new_neighbors.append(t)
+        neighbors = new_neighbors
+          
+        # Down is center.
+        if location + Point(0,1) == self.center:
+          neighbors.extend([(1, Point(p, 0)) for p in range(self.cols)])
+        # Up is center.
+        elif location + Point(0, -1) == self.center:
+          neighbors.extend([(1, Point(p, self.rows - 1)) for p in range(self.cols)])
+        # Right is center.
+        elif location + Point(1,0) == self.center:
+          neighbors.extend([(1, Point(0, p)) for p in range(self.rows)])
+        # Left is center.
+        elif location + Point(-1,0) == self.center:
+          neighbors.extend([(1, Point(self.cols - 1, p)) for p in range(self.rows)])
+        self.neighbors_for_pos[str(location)] = neighbors 
+  
+  def print_neighbors_for_pos(self):
+    for location in self.neighbors_for_pos:
+      n = self.neighbors_for_pos[location]
+      print(location, ' (', len(n),  '):', n)
+
+  def get(self, location, level):
+    return self.levels[level].get(location)
+
+  def set(self, location, level, value):
+    self.levels[level].set(location, value)
+
+  def extend_levels(self):
+    high = max(self.levels.keys()) + 1
+    low = min(self.levels.keys()) - 1
+    _ = self.levels[high]
+    _ = self.levels[low]
+
+  def get_levels_range(self):
+    return range(min(self.levels.keys()), max(self.levels.keys()) + 1)
+
+  def count_neighbors(self, location, level):
     neighbors = 0
-    for d in DIRECTIONS:
-      n = self.get(location+d)
-      if n == '#':
+    for dl, location in self.neighbors_for_pos[str(location)]:
+      if self.get(location, level + dl) == '#':
         neighbors += 1
-      elif n == '?':
-        # level up in the hierarchy and count.
     return neighbors
 
