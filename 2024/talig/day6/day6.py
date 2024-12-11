9fc81439417f03d6b8b8ac8b2df73926e85cc19e
TaliG <gutmant@gmail.com>
2024-12-11 08:27:08 +0200

    finishing day6

--- 2024/talig/day6/day6.py
+++ 2024/talig/day6/day6.py
@@ -31,26 +31,18 @@ class Point:
   def __eq__(self, p):
     return self.x == p.x and self.y == p.y
 
-  def between(self, a, b):
-    if a.x == b.x:
-      if a.y> b.y:
-        return self.y> b.y and self.y < a.y
-      elif a.y < b.y:
-        return self.y< b.y and self.y > a.y
-    elif a.y == b.y:
-      if a.x> b.x:
-        return self.x> b.x and self.x < a.x
-      elif a.x < b.x:
-        return self.x< b.x and self.x > a.x
-    return False
+  def max(self, n):
+    return Point(max(self.x, n.x), max(self.y, n.y))
 
+  def min(self, n):
+    return Point(min(self.x, n.x), min(self.y, n.y))
 
 SIGNS = ['^', '>','v','<']
 # up, right, down, left
 STEPS = [Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)]
  
-    
-VISITED = set(['X', '|','-','+'])
+OBSTACLES = set(['#','O'])
+VISITED = 'X'
 
 class Board():
 
@@ -63,9 +55,9 @@ class Board():
     self.findGuard()
     self.visited = 1
     self.turns = {'rows': {}, 'cols': {}}
-    self.turnsOrder = []
-    self.mark = '|'
-    self.just_turned = False
+    self.turnsOrder = set()
+    self.bottomRight = Point(0,0)
+    self.topLeft = Point(len(self.board), len(self.board[0]))
 
   def findGuard(self):
     for r in range(len(self.board)):
@@ -80,15 +72,10 @@ class Board():
   def turnRight(self):
     self.guard = (self.guard + 1) % len(SIGNS) 
     p = self.guard_location
-    if (p, self.guard) not in self.turnsOrder:
-      self.turnsOrder.append((p, self.guard))
-    else:
-      return True # loop
-    self.just_turned = True
-    if self.mark == '|': 
-      self.mark = '-'
-    else:
-      self.mark = '|'
+    # if this point is already in the list and in the same direction, we have a loop
+    if (p, self.guard) in self.turnsOrder:
+      return True
+    self.turnsOrder.add((p, self.guard))
     return False
 
   def isVisited(self, n):
@@ -97,29 +84,29 @@ class Board():
   def isOutOfRange(self, n):
     return n.y < 0 or n.x < 0 or n.y >= len(self.board) or n.x >= len(self.board[0])
 
+  def updateBox(self,n):
+    self.topLeft = self.topLeft.min(n.add(Point(-1,-1)))
+    self.bottomRight = self.bottomRight.max(n.add(Point(1,1)))
+
   def advanceOne(self):
     isLoop = False
     n = self.guard_location.add(STEPS[self.guard])
-    if not self.isOutOfRange(n) and self.isObstacle(n):
-      isLoop = self.turnRight()
-      n = self.guard_location.add(STEPS[self.guard])
-
-    if self.isOutOfRange(n) or isLoop:
+    if self.isOutOfRange(n):
       return False, isLoop
+    if self.isObstacle(n):
+      isLoop = self.turnRight()
+      return True, isLoop
     return self.visit(self.guard_location, n), isLoop
 
   def isObstacle(self, p):
-    return self.get(p) in  ['#', 'O']
+    return self.get(p) in OBSTACLES
 
   def visit(self,p, n):
     if not self.isVisited(n):
       self.visited += 1
-    sign = self.mark
-    if self.just_turned:
-      sign = '+'
-      self.just_turned = False
-    self.board[p.y][p.x] = sign
+    self.board[p.y][p.x] = VISITED
     self.board[n.y][n.x] = SIGNS[self.guard]
+    self.updateBox(n)
     self.guard_location = n
     return True
 
@@ -139,44 +126,45 @@ class Board():
 def addObstacle(board, row, col):
   p = Point(col, row)
   loop = False
-  if not board.isObstacle(p): 
+  if not board.isObstacle(p) and not p == board.guard_location: 
     board.board[row][col] = 'O'
     inBounds, loop = board.advanceOne()
     while inBounds and not loop:
       inBounds, loop = board.advanceOne()
-    #if loop:
   return loop
 
-def bruteforce(board):
+def bruteforce(board, tl, br):
   options = []
-  c = deepcopy(board)
+  reset = deepcopy(board)
   for row in range(len(board.board)):
     for col in range(len(board.board[0])):
-      loop = addObstacle(board, row, col)
+      b = deepcopy(board)
+      loop = addObstacle(b, row, col)
       if loop:
         options.append(Point(row, col))
-      board = deepcopy(c)
-  return options
+        reset.board[row][col] = 'O'
+  return options, reset
 
 def traverse(board):
-  while board.advanceOne():
+  noor, _ = board.advanceOne()
+  while noor:
+    noor, _ = board.advanceOne()
     continue
+  
  
 def main():
   f = open(sys.argv[1])
   b = Board(f.readlines())
   f.close()
   reset = deepcopy(b)
-  #traverse(b)
-  print(reset)
-  
+  traverse(b)
+  #print(reset)
   
   print("Result part1:", b.countVisited())
   # Part 2:
-  options = bruteforce(reset)
-  print(options)
+  options, obsts = bruteforce(reset, b.topLeft, b.bottomRight)
   print("Result part2:", len(options))
-  # Result part2: 1577 / submit when there's wifi. too low
+  # Result part2: 1703
 
 
 
